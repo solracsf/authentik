aeddf867887dfe46fe3297f84be5608bb2bf7af8
Jens Langhammer <jens@goauthentik.io>
2023-09-27 13:41:30 +0200

    add references to generic source and provider sections
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/sidebars.js
+++ website/sidebars.js
@@ -59,6 +59,12 @@ const docsSidebar = {
         {
             type: "category",
             label: "Providers",
+            link: {
+                type: "generated-index",
+                title: "Providers",
+                slug: "providers",
+                description: "Overview of all available provider types",
+            },
             items: [
                 {
                     type: "category",
